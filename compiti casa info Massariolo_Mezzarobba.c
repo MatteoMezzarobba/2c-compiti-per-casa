26bdd5504de3a6badfccffd37634591a066d296e
MatteoMezzarobba <m.mezzarobba24@gmail.com>
2017-04-08 10:27:06 +0200

    Add files via upload

---
+++ compiti casa info Massariolo_Mezzarobba.c
@@ -0,0 +1,331 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+      char a
+
+      // qual è il valore massimo che si può rappresentare su 8 bit?
+      // Quale è il minimo?
+
+      a = 255;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a+1;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a-1;
+      cout << "La variabile a ha valore: " << a << endl;
+      return 0;
+      }
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+      char16_t a
+
+      // qual è il valore massimo che si può rappresentare su 16 bit?
+      // Quale è il minimo?
+
+      a = 65535;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a+1;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a-1;
+      cout << "La variabile a ha valore: " << a << endl;
+      return 0;
+      }
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    char32_t a
+
+      // qual è il valore massimo che si può rappresentare su 32 bit?
+      // Quale è il minimo?
+
+      a = 4294967295;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a+1;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a-1;
+      cout << "La variabile a ha valore: " << a << endl;
+      return 0;
+      }
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    wchar_t a
+
+      // qual è il valore massimo che si può rappresentare su 64 bit?
+      // Quale è il minimo?
+
+      a = ;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a+1;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a-1;
+      cout << "La variabile a ha valore: " << a << endl;
+      return 0;
+      }
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+     signed char a
+
+      // qual è il valore massimo che si può rappresentare su 8 bit?
+      // Quale è il minimo?
+
+      a = 127;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a+1;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a-1;
+      cout << "La variabile a ha valore: " << a << endl;
+      return 0;
+      }
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    signed short int a
+
+      // qual è il valore massimo che si può rappresentare su 16 bit?
+      // Quale è il minimo?
+
+      a = 32767;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a+1;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a-1;
+      cout << "La variabile a ha valore: " << a << endl;
+      return 0;
+      }
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+     signed int a
+
+      // qual è il valore massimo che si può rappresentare su 16 bit?
+      // Quale è il minimo?
+
+      a = 32767;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a+1;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a-1;
+      cout << "La variabile a ha valore: " << a << endl;
+      return 0;
+      }
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    signed long int a
+
+      // qual è il valore massimo che si può rappresentare su 32 bit?
+      // Quale è il minimo?
+
+      a = 2147483647;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a+1;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a-1;
+      cout << "La variabile a ha valore: " << a << endl;
+      return 0;
+      }
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    signed long long int a
+
+      // qual è il valore massimo che si può rappresentare su 64 bit?
+      // Quale è il minimo?
+
+      a = 9223372036854775807;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a+1;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a-1;
+      cout << "La variabile a ha valore: " << a << endl;
+      return 0;
+      }
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    unsigned char a
+
+      // qual è il valore massimo che si può rappresentare su 8 bit?
+      // Quale è il minimo?
+
+      a = 255;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a+1;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a-1;
+      cout << "La variabile a ha valore: " << a << endl;
+      return 0;
+      }
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    unsigned short int a
+
+      // qual è il valore massimo che si può rappresentare su 16 bit?
+      // Quale è il minimo?
+
+      a = 65535;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a+1;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a-1;
+      cout << "La variabile a ha valore: " << a << endl;
+      return 0;
+      }
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    unsigned int a
+
+      // qual è il valore massimo che si può rappresentare su 16 bit?
+      // Quale è il minimo?
+
+      a = 4294967295;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a+1;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a-1;
+      cout << "La variabile a ha valore: " << a << endl;
+      return 0;
+      }
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    unsigned long int a
+
+      // qual è il valore massimo che si può rappresentare su 16 bit?
+      // Quale è il minimo?
+
+      a = 4294967295;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a+1;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a-1;
+      cout << "La variabile a ha valore: " << a << endl;
+      return 0;
+      }
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    unsigned long long int a
+
+      // qual è il valore massimo che si può rappresentare su 32 bit?
+      // Quale è il minimo?
+
+      a = 18446744073709551615;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a+1;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a-1;
+      cout << "La variabile a ha valore: " << a << endl;
+      return 0;
+      }
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    float a
+
+      // qual è il valore massimo che si può rappresentare su 32 bit?
+      // Quale è il minimo?
+
+      a = 3,4*10°38;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a+1;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a-1;
+      cout << "La variabile a ha valore: " << a << endl;
+      return 0;
+      }
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    double a
+
+      // qual è il valore massimo che si può rappresentare su 64 bit?
+      // Quale è il minimo?
+
+      a = 1,8*10°308;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a+1;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a-1;
+      cout << "La variabile a ha valore: " << a << endl;
+      return 0;
+      }
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    long double a
+
+      // qual è il valore massimo che si può rappresentare su 64 bit?
+      // Quale è il minimo?
+
+      a = 1,8*10°308;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a+1;
+      cout << "La variabile a ha valore: " << a << endl;
+      a = a-1;
+      cout << "La variabile a ha valore: " << a << endl;
+      return 0;
+      }
